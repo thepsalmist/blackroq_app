6f25182f7f664ed2d26b8bf541bf21529b7bc32b
thepsalmist <xf.xavierfrank@gmail.com>
2019-12-04 09:48:25 +0300

    modify settings

--- blackroq/settings.py
+++ blackroq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "0e1f354c5d3e455e34bac48fbce7b29b2f478647c6075d24"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["178.79.156.68"]
+ALLOWED_HOSTS = ["127.0.0.1", "178.79.156.68"]
 
 
 # Application definition
@@ -129,10 +129,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
-# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-# AWS_S3_REGION_NAME = "us-east-2"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "us-east-2"
 
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
