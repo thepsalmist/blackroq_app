f152bf18ccfca17d66b939b3a564a2b7fbc4357e
thepsalmist <xf.xavierfrank@gmail.com>
2021-05-31 15:42:30 +0300

    modify settings

--- blackroq/settings/base.py
+++ blackroq/settings/base.py
@@ -27,6 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "core.apps.CoreConfig",
     "blog.apps.BlogConfig",
     "marketting.apps.MarkettingConfig",
@@ -36,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "crispy_forms",
     "taggit",
